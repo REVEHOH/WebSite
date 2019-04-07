bc70edfd30db67106eb1448d3a13b6c9c1d3b139
REVEHOH <chenxiaohui040709@163.com>
2019-04-07 21:24:48 +0800

    first commit.

---
+++ js&jq/button.js
@@ -0,0 +1,25 @@
+function test(){
+		 var url =new Array();
+		 url[0]="3.1.1.htm";
+		 url[1]="3.1.2.htm";
+		 url[2]="3.1.3.htm";
+		 url[3]="3.1.4.htm";
+		 url[4]="3.1.5.htm";
+		 url[5]="3.2.1.htm";
+		 url[6]="3.2.2.htm";
+		 url[7]="3.2.3.htm";
+		 url[8]="3.2.4.htm";
+		 url[9]="3.2.5.htm";
+		 url[10]="3.3.1.htm";
+		 url[11]="3.3.2.htm";
+		 url[12]="3.3.3.htm";
+		 url[13]="3.3.4.htm";
+		 url[14]="3.3.5.htm";
+		 url[15]="3.4.1.htm";
+		 url[16]="3.4.2.htm";
+		 url[17]="3.4.3.htm";
+		 url[18]="3.4.4.htm";
+		 url[19]="3.4.5.htm";
+		 var ints=parseInt(Math.random()*(url.length));
+		 window.open(url[ints]);
+	 }
